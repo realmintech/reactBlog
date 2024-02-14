b1f3e236e9db1bd19eec1aa136fbe32c9347eb56
wastech <fataiwasiu2@gmail.com>
2024-02-14 22:25:13 +0100

    Setup nestjs :joy:

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,24 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/home/Index';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import About from './pages/about/About';
 import Blogs from './pages/blogs/Blogs';
 import Contact from './pages/contact/Contact';
 import Navbar from './components/common/navbar/Navbar';
 import Footer from './components/common/footer/Footer';
 
-
 export default function App() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Navbar />
-            <Routes>
-                <Route exact path='/' Component={ Home } />
-                <Route path='/about'  Component={ About } />
-                <Route path='/blogs'  Component={ Blogs } />
-                <Route path='/contact' Component={ Contact } />
-            </Routes>
-          <Footer />
-        </BrowserRouter>
-      </div>
-    );
-  }
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' Component={Home} />
+          <Route path='/about' Component={About} />
+          <Route path='/blogs' Component={Blogs} />
+          <Route path='/contact' Component={Contact} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
