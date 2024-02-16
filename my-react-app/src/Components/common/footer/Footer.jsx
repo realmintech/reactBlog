4852d2c88e62dfb688084fb969dc339dfc784b0a
Mariam Adesina <adesinamariam7@gmail.com>
2024-02-16 14:33:46 -0800

    bootstrap initiation

--- my-react-app/src/Components/common/footer/Footer.jsx
+++ my-react-app/src/Components/common/footer/Footer.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
-import "./Footer.css"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Logo from '../../../assets/logo.png';
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <>
-      <div className='bootstrap'>
-        <div className='col'><h1><em> Let's Blog</em></h1></div>
-        <div className='col'>
-          <h2>SUBCRIPTION</h2>
-          <input type="text" placeholder='email address' />
-          <input type="text" placeholder='SUBCRIBE' id='input2' />
-        </div>
-        <div className='col'>3</div>
-        <div className='col'>4</div>
-        <hr />
-      </div>
-    </>
-  )
+    <footer className="bg-dark text-light py-4">
+      <Container>
+        <Row>
+          <Col sm={6} md={4} lg={3}>
+          <img src={Logo} alt="letsBlog" className='text-white' text="white" />
+          </Col>
+          <Col sm={6} md={4} lg={3}>
+            <strong>SUBCRIPTION</strong>
+            <input type="email" name="email" id="email" placeholder='Your email address' border="none"/>
+            <button type="submit" className='my-3 mx-4 px-3 text-center'>SUBCRIBE</button>
+          </Col>
+          <Col sm={6} md={4} lg={3}>
+            <h5>Recent posts</h5>
+          </Col>
+          <Col sm={6} md={4} lg={3}>
+            <h5>Our company</h5>
+            <ul className="list-unstyled">
+              <li><a href="/">Home</a></li>
+              <li><a href="/About">About</a></li>
+              <li><a href="/Blogs">Blogs</a></li>
+              <li><a href="/Contact">Contact</a></li>
+            </ul>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <hr />
+            <p>&copy; 2024 Let's Blog. All rights reserved.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
+
+export default Footer;
