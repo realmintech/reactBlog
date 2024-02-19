8e7a49b9e0ef6a64987d3b48077aa63f0b90591c
wastech <fataiwasiu2@gmail.com>
2024-02-19 17:03:35 +0100

    add user resources and implement auth business logic and database access :joy:

--- server/src/auth/dto/create-auth.dto.ts
+++ server/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,27 @@
-export class CreateAuthDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateAuthDto {
+  @ApiProperty({ description: 'Username' })
+  readonly username: string;
+
+  @ApiProperty({ description: 'Email address', example: 'user@example.com' })
+  readonly email: string;
+
+  @ApiProperty({ description: 'Password' })
+  readonly password: string;
+
+
+  @ApiProperty({ description: 'confirmPassword' })
+  readonly confirmPassword: string;
+
+
+  @ApiProperty({ description: 'User role' })
+  readonly role: string;
+
+  @ApiProperty({ description: 'isLoggedIn' })
+  readonly isLoggedIn:boolean
+
+  @ApiProperty({ description: 'Timestamps' })
+  readonly timestamps: Date;
+}
---
+++ server/src/database/mongoose.config.ts
@@ -0,0 +1,5 @@
+import { MongooseModule } from '@nestjs/mongoose';
+export function mongooseConfig() {
+  const mongodbUri = process.env.MONGODB_URI_LOCAL;
+  return MongooseModule.forRoot(mongodbUri);
+}
