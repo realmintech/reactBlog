461fe0fe3988d009a1e38c43bad84b4dff8152c6
wastech <fataiwasiu2@gmail.com>
2024-02-20 17:42:28 +0100

    add validation method to create-auth dto file :joy:

--- server/src/auth/dto/create-auth.dto.ts
+++ server/src/auth/dto/create-auth.dto.ts
@@ -1,14 +1,21 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength, MaxLength, Matches } from 'class-validator';
 
 export class CreateAuthDto {
-  @ApiProperty({ description: 'Username' })
+  @IsNotEmpty({ message: 'Username should not be empty' })
   readonly username: string;
 
+  @IsEmail({}, { message: 'Invalid email format' })
   readonly email: string;
 
+  @IsNotEmpty({ message: 'Password should not be empty' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MaxLength(20, { message: 'Password cannot be longer than 20 characters' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak',
+  })
   readonly password: string;
 
+  @IsNotEmpty({ message: 'Confirm Password should not be empty' })
   readonly confirmPassword: string;
 
   readonly role: string;
@@ -17,11 +24,12 @@ export class CreateAuthDto {
 
   readonly timestamps: Date;
 
+  // These fields are optional and might not need validation
   readonly resetPasswordToken?: string;
 
   readonly resetPasswordExpires?: Date;
 
-  readonly confirmationToken: string;
+  readonly confirmationToken?: string;
 
-  readonly isConfirmed: boolean;
+  readonly isConfirmed?: boolean;
 }
