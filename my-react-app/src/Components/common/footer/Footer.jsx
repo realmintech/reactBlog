9145a288a5713de9bfe17cdd07f1ae36211ec4f8
Mariam Adesina <adesinamariam7@gmail.com>
2024-03-11 08:55:04 +0100

    check my project boss

--- my-react-app/src/Components/common/footer/Footer.jsx
+++ my-react-app/src/Components/common/footer/Footer.jsx
@@ -8,74 +8,82 @@ import Logo from '../../../assets/footerLogo.png';
 const Footer = () => {
   return (
     <>
-    <footer className="bg-dark text-light py-4">
-      <Container fluid>
-        <Row>
-          <Col sm={6} md={4} lg={3}>
-            <img
-              src={Logo}
-              alt="letsBlog"
-              className="text-white"
-              text="white"
-            />
-          </Col>
-          <Col sm={6} md={4} lg={3}>
-            <strong>SUBCRIPTION</strong>
-            <br />
-            <div>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Your email address"
-                border="none"
-                className="subcribeInput text-center"
+      <footer className="bg-dark text-light py-4">
+        <Container fluid>
+          <Row>
+            <Col sm={6} md={4} lg={3}>
+              <img
+                src={Logo}
+                alt="letsBlog"
+                className="text-white logo"
+                text="white"
               />
+            </Col>
+            <Col sm={6} md={4} lg={3}>
+              <strong>SUBCRIPTION</strong>
               <br />
-              <input
-                className="footer_subcribe mt-3 text-center"
-                placeholder="SUBCRIBE"
-              />
-            </div>
-          </Col>
-          <Col sm={6} md={4} lg={3}>
-            <h5>Recent posts</h5>
-            <p>
-              BEAUTY OF NATURE <br /> <em>July 22, 2015</em>{" "}
-            </p>
-            <p>
-              FASHION MODEL SHOOT <br /> <em>July 22, 2015</em>
-            </p>
-            <p>
-              GOLDEN SNOW LAND <br /> <em>July 22, 2015</em>
-            </p>
-          </Col>
-          <Col sm={6} md={4} lg={3}>
-            <h5>Our company</h5>
-            <ul className="list-unstyled">
-              <li>
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Your email address"
+                  border="none"
+                  className="subcribeInput text-center"
+                />
+                <br />
+                <input
+                  className="footer_subcribe mt-3 text-center"
+                  placeholder="SUBCRIBE"
+                />
+              </div>
+            </Col>
+            <Col sm={6} md={4} lg={3}>
+              <h5>Recent posts</h5>
+              <p>
+                BEAUTY OF NATURE <br /> <em>July 22, 2015</em>{" "}
+              </p>
+              <p>
+                FASHION MODEL SHOOT <br /> <em>July 22, 2015</em>
+              </p>
+              <p>
+                GOLDEN SNOW LAND <br /> <em>July 22, 2015</em>
+              </p>
+            </Col>
+            <Col sm={6} md={4} lg={3}>
+              <h5>Our company</h5>
+              <ul className="list-unstyled footer_links">
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/About">About</a>
+                </li>
+                <li>
+                  <a href="/Blogs">Blogs</a>
+                </li>
+                <li>
+                  <a href="/Contact">Contact</a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="footerLine">
+              <div className='copy_right'>
+                <p>&copy; 2024 Let's Blog. All rights reserved.</p>
+              </div>
+              <div className='footer_link'>
                 <a href="/">Home</a>
-              </li>
-              <li>
                 <a href="/About">About</a>
-              </li>
-              <li>
-                <a href="/Blogs">Blogs</a>
-              </li>
-              <li>
+                <a href="/Blogs">Blog</a>
                 <a href="/Contact">Contact</a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-        <Row>
-          <Col className='footerLine'>
-            <p>&copy; 2024 Let's Blog. All rights reserved.</p>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
-  </>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
   );
 }
 
