597d8aa1aff07edd33be9deecd4bbbbb2985e215
Mariam Adesina <adesinamariam7@gmail.com>
2024-09-04 00:25:36 +0100

    category component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -53,7 +53,7 @@ export default function App() {
             }
           />
           <Route
-            path="/blog/id"
+            path="/blog/:id"
             element={
               <LayoutWithNavbarAndFooter>
                 <Blog />
