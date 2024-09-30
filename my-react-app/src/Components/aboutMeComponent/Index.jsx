a5e271e115a7b37dc81c5d52f82758c21b08dddd
Mariam Adesina <adesinamariam7@gmail.com>
2024-09-30 16:53:18 +0100

    debugging

--- my-react-app/src/Components/aboutMeComponent/Index.jsx
+++ my-react-app/src/Components/aboutMeComponent/Index.jsx
@@ -1,17 +1,17 @@
-import BeautyIdol from "../../assets/carousel2.png";
-import "./Index.css";
+import MyPicture from '../../assets/passport.jpg';
+import './Index.css';
 export default function AboutMe() {
   return (
     <>
       <div className="card w-400 mt-3">
         <div className="card-body">
           <h2 className="card-title text-center">ABOUT ME</h2>
-          <img className="card__img__top" src={BeautyIdol} alt="Card" />
+          <img className="card__img__top" src={MyPicture} alt="Card" />
           <p className="card-text">
-            Some example text some example text. John Doe is an architect and
-            engineer
+            I am a front-end developer, passionate about converting ideas into
+            reality.
           </p>
-          <em className="px-5">Lorem ipsum dolor sit</em>
+          <em className="px-5">System administrator</em>
         </div>
       </div>
     </>
