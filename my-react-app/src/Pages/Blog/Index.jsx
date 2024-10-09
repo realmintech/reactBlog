6bb4485ed47edc80f6aa9ec4509f1e50cdbb0968
wastech <fataiwasiu2@gmail.com>
2024-10-09 17:44:41 +0100

    :zap:

--- my-react-app/src/Pages/Blog/Index.jsx
+++ my-react-app/src/Pages/Blog/Index.jsx
@@ -15,12 +15,10 @@ import { useDispatch, useSelector } from 'react-redux';
 export default function SingleBlogPost() {
   const { id } = useParams();
   const [blog, setBlog] = useState(null);
-  const [comments, setComments] = useState([]);
   const [content, setContent] = useState('');
   const dispatch = useDispatch();
 
   const data = useSelector((state) => state.comments.comments);
-  console.log('Comment data: ', data);
 
   useEffect(() => {
     const fetchBlogDetail = async () => {
@@ -30,7 +28,6 @@ export default function SingleBlogPost() {
         );
         if (blogDetail && blogDetail.data) {
           setBlog(blogDetail.data);
-          setComments(blogDetail.data.comments || []);
         }
       } catch (err) {
         console.log('this is blog detail: ', err);
@@ -44,10 +41,10 @@ export default function SingleBlogPost() {
     dispatch(createComment(id, content));
     setContent('');
   };
-  
+
   useEffect(() => {
     dispatch(getPostComments(id));
-  });
+  }, [dispatch, id]);
 
   if (!blog) {
     return <div>Loading...</div>;
@@ -55,34 +52,34 @@ export default function SingleBlogPost() {
 
   return (
     <>
-      <div className="bgImg"></div>
-      <div className="container">
-        <div className="row">
-          <div className="col4-lg-8 col-md-8 col-sm-8">
-            <div className="heading">
+      <div className='bgImg'></div>
+      <div className='container'>
+        <div className='row'>
+          <div className='col4-lg-8 col-md-8 col-sm-8'>
+            <div className='heading'>
               <h3>{blog.title}</h3>
-              <p className="title_date">
+              <p className='title_date'>
                 {moment(blog.timestamp).format('llll')}
               </p>
             </div>
-            <div className="news">
-              <div className="blog_big_img">
+            <div className='news'>
+              <div className='blog_big_img'>
                 <img
                   src={blog.imageUrl}
                   alt={blog.title}
-                  className="beautyPic"
+                  className='beautyPic'
                 />
               </div>
-              <p className="picCaption">Tags: {blog.tags}</p>
+              <p className='picCaption'>Tags: {blog.tags}</p>
               <p>{blog.description}</p>
-              <div className="tagShare row">
-                <div className="btns col-lg-8 col-md-8 col-sm-8">
+              <div className='tagShare row'>
+                <div className='btns col-lg-8 col-md-8 col-sm-8'>
                   <FaTag />
-                  <button className="btn">COOKING</button>
-                  <button className="btn">FOOD</button>
-                  <button className="btn">LIFESTYLE</button>
+                  <button className='btn'>COOKING</button>
+                  <button className='btn'>FOOD</button>
+                  <button className='btn'>LIFESTYLE</button>
                 </div>
-                <div className="shareIcon col-lg-4 col-md-4 col-sm-4">
+                <div className='shareIcon col-lg-4 col-md-4 col-sm-4'>
                   <button>
                     <FaShare />
                     Share
@@ -91,18 +88,18 @@ export default function SingleBlogPost() {
               </div>
             </div>
             <div>
-              <p className="highLight">
+              <p className='highLight'>
                 <span> ART / FOOD / TOP</span>
               </p>
-              <span className="flexImgWord row">
-                <div className="col-lg-2 col-md-2 col-sm-2">
+              <span className='flexImgWord row'>
+                <div className='col-lg-2 col-md-2 col-sm-2'>
                   <img
                     src={ManOnSuit}
-                    alt="man_on_suit"
-                    className="roundedImg"
+                    alt='man_on_suit'
+                    className='roundedImg'
                   />
                 </div>
-                <span className="col-lg-10 col-md-10 col-sm-10">
+                <span className='col-lg-10 col-md-10 col-sm-10'>
                   <h3>John Phillipe</h3>
                   <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -114,17 +111,17 @@ export default function SingleBlogPost() {
               </span>
               <YouMightAlsoLikeComponent />
               <p
-                className="comment"
+                className='comment'
                 style={{
                   borderTop: '1px solid rgba(139,135,135,0.514)',
                   paddingTop: '30px',
                 }}
               >
-                {comments.length} COMMENTS
+                {data.length} COMMENTS
               </p>
-              {comments.length > 0 ? (
-                comments.map((comment, index) => (
-                  <div className="flexImgWord row" key={index}>
+              {data.length > 0 ? (
+                data.map((comment, index) => (
+                  <div className='flexImgWord row' key={index}>
                     <Comment item={comment} />
                   </div>
                 ))
@@ -132,29 +129,29 @@ export default function SingleBlogPost() {
                 <p>No comments available.</p>
               )}
 
-              <p className="commentSection">
+              <p className='commentSection'>
                 Your email address will not be published. Required fields are
                 marked *
               </p>
               <h6>COMMENT *</h6>
               <textarea
-                name="comment"
-                id=""
-                cols="97"
-                rows="5"
-                className="textArea"
+                name='comment'
+                id=''
+                cols='97'
+                rows='5'
+                className='textArea'
                 value={content}
                 onChange={(e) => setContent(e.target.value)}
               ></textarea>
-              <button className="btnPost" onClick={handleSubmit}>
+              <button className='btnPost' onClick={handleSubmit}>
                 POST COMMENT
               </button>
             </div>
           </div>
-          <div className="col-lg-4 col-md-4 col-sm-6">
+          <div className='col-lg-4 col-md-4 col-sm-6'>
             <AboutMe />
             <Newsletter />
-            <div className="tagDiv">
+            <div className='tagDiv'>
               <button>BRIDGE</button>
               <button>PHOTOGRAPH</button>
               <button>FOOD</button>
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -3,13 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './store'
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
